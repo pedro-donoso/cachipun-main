ea9d76808a32e1b1205b1ff5367cb693a25e5cc1
pedro <pedro.donoso.r@usach.cl>
2021-07-09 22:11:01 -0400

    agrego opciones

--- assets/script/scripts.js
+++ assets/script/scripts.js
@@ -10,11 +10,9 @@ function comprobarSeleccionado() {
     }
 
     if (radioActivo == null) {
-        alert("Por favor seleccionar una opción")
+        alert("Debes marcar 1 opción")
     }
-    /* else {
-        document.getElementById("salida").innerHTML = radioActivo
-    } */
+    
     
     let jugada_usuario = 1
     jugada_computador_num = GenerarJugadaComputador()
@@ -23,40 +21,40 @@ function comprobarSeleccionado() {
     jugada_tu_num = parseInt(radioActivo)
     jug_tu = conversion(parseInt(jugada_tu_num))
 
-    document.getElementById("jugadashechas").innerHTML = "Computador:" + jug_computador + "<br>Tu:" + jug_tu + "<br>"
+    document.getElementById("jugadashechas").innerHTML = "MÁQUINA:" + jug_computador + "<br>HUMANO:" + jug_tu + "<br>"
     comp2 = comprobarGanador(jugada_computador_num, jugada_tu_num)
 
 }
 
 
-function comprobarGanador(comp, tu) { //funcion que compara las dos elecciones por los numeros 
-    if (comp == tu) { document.getElementById("resultado").innerHTML = 'Esto ha sido un empate' }
+function comprobarGanador(comp, tu) { 
+    if (comp == tu) { document.getElementById("resultado").innerHTML = 'EMPATE' }
     else {
         if ((comp == '0' && tu == '2') || (comp == '1' && tu == '0') || (comp == '2' && tu == '0')) {
-            document.getElementById("resultado").innerHTML = 'Lamentablemente has perdido'
+            document.getElementById("resultado").innerHTML = '¡PERDISTE!'
         }
         else {
-            document.getElementById("resultado").innerHTML = 'Felicitaciones, has ganado!!!!'
+            document.getElementById("resultado").innerHTML = '¡GANASTE!'
         }
     }
 }
 
-function conversion(num) { //funcion que covierte el numero a eleccion en string
+function conversion(num) { 
     switch (num) {
         case 0:
-            salida = 'Piedra';
+            salida = 'PIEDRA';
             break;
         case 1:
-            salida = 'Papel';
+            salida = 'PAPEL';
             break;
         case 2:
-            salida = 'Tijera';
+            salida = 'TIJERA';
             break;
     }
     return salida
 }
 
-function GenerarJugadaComputador() { //funcion que generar la jugada del computador
+function GenerarJugadaComputador() { 
     var jugada = (Math.round(Math.random() * 2))
     return jugada
 }
